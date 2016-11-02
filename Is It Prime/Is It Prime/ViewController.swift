5f293c55a6dda1fe66cc598db1dcde2122dd0d1d
Joseph Chen <josephchen@josephs-mbp.lan>
2016-11-01 17:04:26 -0700

    Hello World and is it prime

---
+++ Is It Prime/Is It Prime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Is It Prime
+//
+//  Created by Joseph Chen on 11/1/16.
+//  Copyright © 2016 Joseph Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
