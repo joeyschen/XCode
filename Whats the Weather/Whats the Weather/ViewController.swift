ab38808e09c5fc089e037549c063d478a17681a3
Joseph Chen <josephchen@Josephs-MacBook-Pro.local>
2016-11-07 22:45:02 -0800

    What's the weather app

---
+++ Whats the Weather/Whats the Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Whats the Weather
+//
+//  Created by Joseph Chen on 11/4/16.
+//  Copyright © 2016 Joseph Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
